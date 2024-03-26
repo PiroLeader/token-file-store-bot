89bb9b8846496d975daa6d33b6df429f5f460dbc
PiroLeader <158609590+PiroLeader@users.noreply.github.com>
2024-03-26 20:21:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "©️ @Students_Helpers")
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", False) == 'True'
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", False) == 'False'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
